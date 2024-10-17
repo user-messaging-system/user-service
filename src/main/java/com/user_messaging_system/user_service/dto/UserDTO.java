ee80055bca5a0fb06c7bbba57bf96259a6324ccb
burakyapici747 <burakyapici747@gmail.com>
2024-10-17 12:05:56 +0300

    feat(user-service): add UserDTO for data transfer in user service

---
+++ src/main/java/com/user_messaging_system/user_service/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.user_messaging_system.user_service.dto;
+
+import java.util.List;
+
+public record UserDTO(
+        String id,
+        String email,
+        String name,
+        String lastName,
+        List<String> roles
+) {}
