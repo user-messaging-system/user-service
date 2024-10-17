56b9d0e66ce9e52f97a6e3ab2812379d52018c35
burakyapici747 <burakyapici747@gmail.com>
2024-10-17 12:16:04 +0300

    refactor(user-controller): use UserMapper for DTO conversion and SuccessResponseBuilder for consistent responses
    - Refactored getById, getByEmail, and register methods to use UserMapper for converting entities to DTOs.
    - Updated the response creation to use SuccessResponseBuilder for a more structured response format.
    - Improved consistency across controller methods by aligning response creation and DTO mapping logic.

--- src/main/java/com/user_messaging_system/user_service/api/UserController.java
+++ src/main/java/com/user_messaging_system/user_service/api/UserController.java
@@ -4,10 +4,14 @@ import com.user_messaging_system.core_library.response.SuccessResponse;
 import com.user_messaging_system.user_service.api.input.UserInput;
 import com.user_messaging_system.user_service.api.output.UserCreateOutput;
 import com.user_messaging_system.user_service.api.output.UserOutput;
+import com.user_messaging_system.user_service.common.SuccessResponseBuilder;
+import com.user_messaging_system.user_service.mapper.UserMapper;
 import com.user_messaging_system.user_service.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/api/users")
@@ -18,32 +22,41 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/{senderId}/{receiverId}")
+    public ResponseEntity<SuccessResponse<List<UserOutput>>> getSenderAndReceiverByIds(
+            @PathVariable String senderId,
+            @PathVariable String receiverId
+    ){
+        SuccessResponse<List<UserOutput>> response = SuccessResponseBuilder.buildSuccessResponse(
+                UserMapper.INSTANCE.userDtoListToUserOutputList(userService.getSenderAndReceiverByIds(senderId, receiverId)),
+                HttpStatus.OK
+        );
+        return ResponseEntity.ok(response);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<SuccessResponse<UserOutput>> getById(@PathVariable(name = "id") String id) {
-        SuccessResponse<UserOutput> response = new SuccessResponse<>(
-                "User fetched successfully",
-                userService.getUserById(id),
-                String.valueOf(HttpStatus.OK)
+        SuccessResponse<UserOutput> response = SuccessResponseBuilder.buildSuccessResponse(
+                        UserMapper.INSTANCE.userDtoToUserOutput(userService.getUserById(id)),
+                        HttpStatus.OK
         );
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/by-email")
     public ResponseEntity<SuccessResponse<UserOutput>> getByEmail(@RequestParam String email){
-        SuccessResponse<UserOutput> response = new SuccessResponse<>(
-                "User fetched successfully",
-                userService.getUserByEmail(email),
-                String.valueOf(HttpStatus.OK)
+        SuccessResponse<UserOutput> response = SuccessResponseBuilder.buildSuccessResponse(
+                UserMapper.INSTANCE.userDtoToUserOutput(userService.getUserByEmail(email)),
+                HttpStatus.OK
         );
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/register")
     public ResponseEntity<SuccessResponse<UserCreateOutput>> register(@RequestBody UserInput userInput){
-        SuccessResponse<UserCreateOutput> response = new SuccessResponse<>(
-                "User registered successfully",
+        SuccessResponse<UserCreateOutput> response = SuccessResponseBuilder.buildSuccessResponse(
                 userService.createUser(userInput),
-                String.valueOf(HttpStatus.CREATED)
+                HttpStatus.CREATED
         );
         return ResponseEntity.ok(response);
     }
