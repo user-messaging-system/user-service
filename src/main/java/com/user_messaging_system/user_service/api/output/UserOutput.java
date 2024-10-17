09f307a476972e7a5c1b8f8b3a03a9636ad8b954
burakyapici747 <burakyapici747@gmail.com>
2024-10-17 12:14:45 +0300

    refactor(user-service): add id field and remove password from UserOutput

--- src/main/java/com/user_messaging_system/user_service/api/output/UserOutput.java
+++ src/main/java/com/user_messaging_system/user_service/api/output/UserOutput.java
@@ -3,9 +3,9 @@ package com.user_messaging_system.user_service.api.output;
 import java.util.List;
 
 public record UserOutput (
+        String id,
         String email,
         String name,
         String lastName,
-        String password,
         List<String> roles
 ) implements UserBaseOutput{}
