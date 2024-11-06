8155da2a3926ff492cecf2ecbf5c06c8c2266405
burakyapici747 <burakyapici747@gmail.com>
2024-11-06 10:07:38 +0300

    feat(user-service): initialize service application

--- src/main/java/com/user_messaging_system/user_service/UserServiceApplication.java
+++ src/main/java/com/user_messaging_system/user_service/UserServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.user_messaging_system.core_library"})
 @EnableDiscoveryClient
 public class UserServiceApplication {
 
