6e845a3551d1a4cf97486277b871f25ea466830b
burakyapici747 <burakyapici747@gmail.com>
2024-11-06 09:56:02 +0300

    feat(api-gateway): add UserController in API gateway for user management.

--- src/main/java/com/user_messaging_system/user_service/api/UserController.java
+++ src/main/java/com/user_messaging_system/user_service/api/UserController.java
@@ -1,9 +1,12 @@
 package com.user_messaging_system.user_service.api;
 
 import com.user_messaging_system.core_library.response.SuccessResponse;
-import com.user_messaging_system.user_service.api.input.UserInput;
-import com.user_messaging_system.user_service.api.output.UserCreateOutput;
-import com.user_messaging_system.user_service.api.output.UserOutput;
+import com.user_messaging_system.user_service.api.input.UserRegisterInput;
+import com.user_messaging_system.user_service.api.input.UserUpdateInput;
+import com.user_messaging_system.user_service.api.output.UserGetCurrentOutput;
+import com.user_messaging_system.user_service.api.output.UserGetOutput;
+import com.user_messaging_system.user_service.api.output.UserUpdateOutput;
+import com.user_messaging_system.user_service.api.output.UserRegisterOutput;
 import com.user_messaging_system.user_service.common.SuccessResponseBuilder;
 import com.user_messaging_system.user_service.mapper.UserMapper;
 import com.user_messaging_system.user_service.service.UserService;
@@ -21,48 +24,74 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
-    @GetMapping("/{senderId}/{receiverId}")
-    public ResponseEntity<SuccessResponse<List<UserOutput>>> getSenderAndReceiverByIds(
-            @PathVariable String senderId,
-            @PathVariable String receiverId
-    ){
-        SuccessResponse<List<UserOutput>> response = SuccessResponseBuilder.buildSuccessResponse(
-                UserMapper.INSTANCE.userDtoListToUserOutputList(userService.getSenderAndReceiverByIds(senderId, receiverId)),
+    @GetMapping("/{id}")
+    public ResponseEntity<SuccessResponse<UserGetOutput>> getById(@PathVariable(name = "id") String id) {
+        SuccessResponse<UserGetOutput> response = SuccessResponseBuilder.buildSuccessResponse(
+                UserMapper.INSTANCE.userDtoToUserGetOutput(userService.getUserById(id)),
                 HttpStatus.OK
         );
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<SuccessResponse<UserOutput>> getById(@PathVariable(name = "id") String id) {
-        SuccessResponse<UserOutput> response = SuccessResponseBuilder.buildSuccessResponse(
-                        UserMapper.INSTANCE.userDtoToUserOutput(userService.getUserById(id)),
-                        HttpStatus.OK
-        );
-        return ResponseEntity.ok(response);
-    }
-
     @GetMapping("/by-email")
-    public ResponseEntity<SuccessResponse<UserOutput>> getByEmail(@RequestParam String email){
-        SuccessResponse<UserOutput> response = SuccessResponseBuilder.buildSuccessResponse(
+    public ResponseEntity<SuccessResponse<UserGetCurrentOutput>> getByEmail(@RequestParam String email){
+        SuccessResponse<UserGetCurrentOutput> response = SuccessResponseBuilder.buildSuccessResponse(
                 UserMapper.INSTANCE.userDtoToUserOutput(userService.getUserByEmail(email)),
                 HttpStatus.OK
         );
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/currentUser")
+    public ResponseEntity<SuccessResponse<UserGetCurrentOutput>> getCurrentUser(
+            @RequestHeader("Authorization") String jwtToken
+    ){
+        SuccessResponse<UserGetCurrentOutput> response = SuccessResponseBuilder.buildSuccessResponse(
+                UserMapper.INSTANCE.userDtoToUserOutput(userService.getCurrentUser(jwtToken)),
+                HttpStatus.OK
+        );
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{senderId}/{receiverId}")
+    public ResponseEntity<SuccessResponse<List<UserGetOutput>>> getSenderAndReceiverByIds(
+            @RequestHeader("Authorization") String jwtToken,
+            @PathVariable String senderId,
+            @PathVariable String receiverId
+    ){
+        SuccessResponse<List<UserGetOutput>> response = SuccessResponseBuilder.buildSuccessResponse(
+                UserMapper.INSTANCE.userDtoListToUserGetOutputList(
+                        userService.getSenderAndReceiverByIds(jwtToken, senderId, receiverId)
+                ),
+                HttpStatus.OK
+        );
+        return ResponseEntity.ok(response);
+    }
+
     @PostMapping("/register")
-    public ResponseEntity<SuccessResponse<UserCreateOutput>> register(@RequestBody UserInput userInput){
-        SuccessResponse<UserCreateOutput> response = SuccessResponseBuilder.buildSuccessResponse(
-                userService.createUser(userInput),
+    public ResponseEntity<SuccessResponse<UserRegisterOutput>> register(@RequestBody UserRegisterInput registerInput){
+        SuccessResponse<UserRegisterOutput> response = SuccessResponseBuilder.buildSuccessResponse(
+                userService.createUser(registerInput),
                 HttpStatus.CREATED
         );
         return ResponseEntity.ok(response);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteById(@PathVariable(name = "id") String id) {
-        return ResponseEntity.ok(null);
+    @PutMapping
+    public ResponseEntity<SuccessResponse<UserUpdateOutput>> updateCurrentUser(
+            @RequestHeader("Authorization") String jwtToken,
+            @RequestBody UserUpdateInput userUpdateInput
+    ){
+        SuccessResponse<UserUpdateOutput> response = SuccessResponseBuilder.buildSuccessResponse(
+                UserMapper.INSTANCE.userDtoToUserUpdateOutput(userService.updateCurrentUser(jwtToken, userUpdateInput)),
+                HttpStatus.OK
+        );
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteCurrentUser(@RequestHeader("Authorization") String jwtToken){
+        userService.deleteCurrentUser(jwtToken);
+        return ResponseEntity.noContent().build();
     }
 }
