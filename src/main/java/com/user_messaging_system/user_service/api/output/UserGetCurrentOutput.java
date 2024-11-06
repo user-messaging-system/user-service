04c4994687b3fb51e67082896a31aea0fc819bfc
burakyapici747 <burakyapici747@gmail.com>
2024-11-06 09:56:30 +0300

    refactor(user-service): rename UserOutput to UserGetCurrentOutput for better clarity.

--- src/main/java/com/user_messaging_system/user_service/api/output/UserGetCurrentOutput.java
+++ src/main/java/com/user_messaging_system/user_service/api/output/UserGetCurrentOutput.java
@@ -2,10 +2,11 @@ package com.user_messaging_system.user_service.api.output;
 
 import java.util.List;
 
-public record UserOutput (
+public record UserGetCurrentOutput(
         String id,
-        String email,
         String name,
         String lastName,
+        String email,
+        String password,
         List<String> roles
 ) implements UserBaseOutput{}
