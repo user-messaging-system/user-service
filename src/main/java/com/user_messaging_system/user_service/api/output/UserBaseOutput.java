67db6c00f566a8208a2cbc30505d7da8ad2de757
burakyapici747 <burakyapici747@gmail.com>
2024-11-06 09:55:43 +0300

    feat(user-service): create UserBaseOutput for shared user response data.

--- src/main/java/com/user_messaging_system/user_service/api/output/UserBaseOutput.java
+++ src/main/java/com/user_messaging_system/user_service/api/output/UserBaseOutput.java
@@ -1,10 +1,7 @@
 package com.user_messaging_system.user_service.api.output;
 
-import java.util.List;
-
 public interface UserBaseOutput {
-    String email();
     String name();
     String lastName();
-    List<String> roles();
+    String email();
 }
