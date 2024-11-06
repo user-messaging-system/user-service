1aaad689e3c49657452cf0023c9e607a8a71b95f
burakyapici747 <burakyapici747@gmail.com>
2024-11-06 10:07:03 +0300

    feat(user-service): add exception handling and utility classes

--- src/main/java/com/user_messaging_system/user_service/mapper/UserMapper.java
+++ src/main/java/com/user_messaging_system/user_service/mapper/UserMapper.java
@@ -1,8 +1,12 @@
 package com.user_messaging_system.user_service.mapper;
 
 import com.user_messaging_system.user_service.api.input.UserInput;
-import com.user_messaging_system.user_service.api.output.UserCreateOutput;
-import com.user_messaging_system.user_service.api.output.UserOutput;
+import com.user_messaging_system.user_service.api.input.UserRegisterInput;
+import com.user_messaging_system.user_service.api.input.UserUpdateInput;
+import com.user_messaging_system.user_service.api.output.UserGetCurrentOutput;
+import com.user_messaging_system.user_service.api.output.UserGetOutput;
+import com.user_messaging_system.user_service.api.output.UserRegisterOutput;
+import com.user_messaging_system.user_service.api.output.UserUpdateOutput;
 import com.user_messaging_system.user_service.dto.UserDTO;
 import com.user_messaging_system.user_service.model.Role;
 import com.user_messaging_system.user_service.model.User;
@@ -17,25 +21,35 @@ public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     @Mapping(target = "roles", source = "roles", qualifiedByName = "mapRoles")
-    UserOutput userToUserOutput(User user);
+    UserGetCurrentOutput userToUserOutput(User user);
 
     @Mapping(target = "roles", source = "user.roles", qualifiedByName = "mapRoles")
     @Mapping(target = "accessToken", source = "accessToken")
-    UserCreateOutput userToUserOutputWithAccessToken(User user, String accessToken);
+    UserRegisterOutput userToUserOutputWithAccessToken(User user, String accessToken);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     @Mapping(target = "mailVerified", ignore = true)
     @Mapping(target = "roles", ignore = true)
     User userInputToUserForUpdate(UserInput user);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    User updateAndReturnUser(UserUpdateInput userUpdateInput, @MappingTarget User existingUser);
+
     @Mapping(target = "mailVerified", ignore = true)
     User userInputToUser(UserInput userInput);
 
+    User userRegisterInputToUser(UserRegisterInput userRegisterInput);
+
     List<UserDTO> userListToUserDTOList(List<User> userList);
 
-    List<UserOutput> userDtoListToUserOutputList(List<UserDTO> userDTOList);
+    List<UserGetCurrentOutput> userDtoListToUserOutputList(List<UserDTO> userDTOList);
+    List<UserGetOutput> userDtoListToUserGetOutputList(List<UserDTO> userDTOList);
 
-    UserOutput userDtoToUserOutput(UserDTO userDTO);
+    UserGetCurrentOutput userDtoToUserOutput(UserDTO userDTO);
+
+    UserGetOutput userDtoToUserGetOutput(UserDTO userDTO);
+
+    UserUpdateOutput userDtoToUserUpdateOutput(UserDTO userDTO);
 
     @Mapping(target = "roles", source = "roles", qualifiedByName = "mapRoles")
     UserDTO userToUserDTO(User user);
