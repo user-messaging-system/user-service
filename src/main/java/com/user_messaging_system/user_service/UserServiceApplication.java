9a5f73625ce22b561d65b12239566a162db2d495
burak <burakyapici747@gmail.com>
2024-11-12 19:42:30 +0300

    fix: explicitly import JWTService in UserServiceApplication

--- src/main/java/com/user_messaging_system/user_service/UserServiceApplication.java
+++ src/main/java/com/user_messaging_system/user_service/UserServiceApplication.java
@@ -1,11 +1,14 @@
 package com.user_messaging_system.user_service;
 
+import com.user_messaging_system.core_library.service.JWTService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = {"com.user_messaging_system.core_library"})
+@SpringBootApplication
 @EnableDiscoveryClient
+@Import(JWTService.class)
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
