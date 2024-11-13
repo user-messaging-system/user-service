4e402928146513cf0fb521eb2f19ccbaad45e042
burak <burakyapici747@gmail.com>
2024-11-13 23:57:07 +0300

    refactor: add @Repository annotation to RoleRepository and UserRepository for better clarity

--- src/main/java/com/user_messaging_system/user_service/repository/UserRepository.java
+++ src/main/java/com/user_messaging_system/user_service/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.user_messaging_system.user_service.repository;
 
 import com.user_messaging_system.user_service.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
