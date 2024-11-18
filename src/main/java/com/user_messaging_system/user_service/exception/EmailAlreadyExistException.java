6dde6fe7575812e8f49274e761bfc19f7650858d
burak <burakyapici747@gmail.com>
2024-11-18 23:16:08 +0300

    feat(user-service): enhance EmailAlreadyExistException class with overloaded constructor

--- src/main/java/com/user_messaging_system/user_service/exception/EmailAlreadyExistException.java
+++ src/main/java/com/user_messaging_system/user_service/exception/EmailAlreadyExistException.java
@@ -4,4 +4,8 @@ public class EmailAlreadyExistException extends RuntimeException {
     public EmailAlreadyExistException(String message) {
         super(message);
     }
+
+    public EmailAlreadyExistException(String message, Throwable cause){
+        super(message, cause);
+    }
 }
