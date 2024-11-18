f19f72ace7ad596d92f2200c85fa9aded8221ca1
burak <burakyapici747@gmail.com>
2024-11-18 23:23:25 +0300

    refactor(user-service): remove UserNotFoundException class

--- src/main/java/com/user_messaging_system/user_service/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.user_messaging_system.user_service.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
