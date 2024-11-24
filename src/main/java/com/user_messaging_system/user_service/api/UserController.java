73d7e9c2a32ad0e188b2f5a89a8b437627a876a4
Burak <burakyapici747@gmail.com>
2024-11-24 21:24:25 +0300

    refactor: update SuccessResponse usage to builder pattern

--- src/main/java/com/user_messaging_system/user_service/api/UserController.java
+++ src/main/java/com/user_messaging_system/user_service/api/UserController.java
@@ -7,7 +7,7 @@ import com.user_messaging_system.user_service.api.output.UserGetCurrentOutput;
 import com.user_messaging_system.user_service.api.output.UserGetOutput;
 import com.user_messaging_system.user_service.api.output.UserUpdateOutput;
 import com.user_messaging_system.user_service.api.output.UserRegisterOutput;
-import com.user_messaging_system.user_service.common.SuccessResponseBuilder;
+import com.user_messaging_system.user_service.common.constant.ApiConstant;
 import com.user_messaging_system.user_service.mapper.UserMapper;
 import com.user_messaging_system.user_service.service.UserService;
 import org.slf4j.Logger;
@@ -18,8 +18,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static com.user_messaging_system.user_service.common.constant.ApiConstant.*;
+
 @RestController
-@RequestMapping("/v1/api/users")
+@RequestMapping(ApiConstant.API_VERSION)
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
@@ -31,11 +33,11 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<SuccessResponse<UserGetOutput>> getById(@PathVariable(name = "id") String id) {
         logger.info("[START] Method getById called in UserController with parameters: id={}", id);
-
-        SuccessResponse<UserGetOutput> response = SuccessResponseBuilder.buildSuccessResponse(
-                UserMapper.INSTANCE.userDtoToUserGetOutput(userService.getUserById(id)),
-                HttpStatus.OK
-        );
+        SuccessResponse<UserGetOutput> response = new SuccessResponse.Builder<UserGetOutput>()
+                .message(USER_SUCCESSFULLY_RETRIEVED_MESSAGE)
+                .data(UserMapper.INSTANCE.userDtoToUserGetOutput(userService.getUserById(id)))
+                .status(HttpStatus.OK.toString())
+                .build();
 
         logger.info("[SUCCESS] Method getById in UserController completed successfully. Response: {}", response);
         return ResponseEntity.ok(response);
@@ -44,11 +46,11 @@ public class UserController {
     @GetMapping("/by-email")
     public ResponseEntity<SuccessResponse<UserGetCurrentOutput>> getByEmail(@RequestParam String email){
         logger.info("[START] Method getByEmail called in UserController with parameters: email={}", email);
-
-        SuccessResponse<UserGetCurrentOutput> response = SuccessResponseBuilder.buildSuccessResponse(
-                UserMapper.INSTANCE.userDtoToUserOutput(userService.getUserByEmail(email)),
-                HttpStatus.OK
-        );
+        SuccessResponse<UserGetCurrentOutput> response = new SuccessResponse.Builder<UserGetCurrentOutput>()
+                .message(USER_SUCCESSFULLY_RETRIEVED_MESSAGE)
+                .data(UserMapper.INSTANCE.userDtoToUserOutput(userService.getUserByEmail(email)))
+                .status(HttpStatus.OK.toString())
+                .build();
 
         logger.info("[SUCCESS] Method getByEmail in UserController completed successfully. Response: {}", response);
         return ResponseEntity.ok(response);
@@ -59,11 +61,11 @@ public class UserController {
         @RequestHeader("Authorization") String jwtToken
     ){
         logger.info("[START] Method getCurrentUser called in UserController with parameters: jwtToken={}", jwtToken);
-
-        SuccessResponse<UserGetCurrentOutput> response = SuccessResponseBuilder.buildSuccessResponse(
-                UserMapper.INSTANCE.userDtoToUserOutput(userService.getCurrentUser(jwtToken)),
-                HttpStatus.OK
-        );
+        SuccessResponse<UserGetCurrentOutput> response = new SuccessResponse.Builder<UserGetCurrentOutput>()
+                .message(USER_SUCCESSFULLY_RETRIEVED_MESSAGE)
+                .data(UserMapper.INSTANCE.userDtoToUserOutput(userService.getCurrentUser(jwtToken)))
+                .status(HttpStatus.OK.toString())
+                .build();
 
         logger.info("[SUCCESS] Method getCurrentUser in UserController completed successfully. Response: {}", response);
         return ResponseEntity.ok(response);
@@ -77,13 +79,13 @@ public class UserController {
     ){
         logger.info("[START] Method getSenderAndReceiverByIds called in UserController with parameters:" +
                 " userId={}, senderId={}, receiverId={}", jwtToken, senderId, receiverId);
-
-        SuccessResponse<List<UserGetOutput>> response = SuccessResponseBuilder.buildSuccessResponse(
-                UserMapper.INSTANCE.userDtoListToUserGetOutputList(
+        SuccessResponse<List<UserGetOutput>> response = new SuccessResponse.Builder<List<UserGetOutput>>()
+                .message(USER_SUCCESSFULLY_RETRIEVED_MESSAGE)
+                .data(UserMapper.INSTANCE.userDtoListToUserGetOutputList(
                         userService.getSenderAndReceiverByIds(jwtToken, senderId, receiverId)
-                ),
-                HttpStatus.OK
-        );
+                ))
+                .status(HttpStatus.OK.toString())
+                .build();
 
         logger.info(
                 "[SUCCESS] Method getSenderAndReceiverByIds in UserController completed successfully. Response: {}",
@@ -95,11 +97,11 @@ public class UserController {
     @PostMapping
     public ResponseEntity<SuccessResponse<UserRegisterOutput>> createUser(@RequestBody UserRegisterInput registerInput){
         logger.info("[START] Method register called in UserController with parameters: registerInput={}", registerInput);
-
-        SuccessResponse<UserRegisterOutput> response = SuccessResponseBuilder.buildSuccessResponse(
-                userService.createUser(registerInput),
-                HttpStatus.CREATED
-        );
+        SuccessResponse<UserRegisterOutput> response = new SuccessResponse.Builder<UserRegisterOutput>()
+                .message(USER_SUCCESSFULLY_REGISTERED_MESSAGE)
+                .data(UserMapper.INSTANCE.userRegisterDTOToUserRegisterOutput(userService.createUser(registerInput)))
+                .status(HttpStatus.CREATED.toString())
+                .build();
 
         logger.info("[SUCCESS] Method register in UserController completed successfully. Response: {}", response);
         return ResponseEntity.ok(response);
@@ -116,11 +118,11 @@ public class UserController {
                 jwtToken,
                 userUpdateInput
         );
-
-        SuccessResponse<UserUpdateOutput> response = SuccessResponseBuilder.buildSuccessResponse(
-                UserMapper.INSTANCE.userDtoToUserUpdateOutput(userService.updateUserById(id, jwtToken, userUpdateInput)),
-                HttpStatus.OK
-        );
+        SuccessResponse<UserUpdateOutput> response = new SuccessResponse.Builder<UserUpdateOutput>()
+                .message(USER_SUCCESSFULLY_UPDATED_MESSAGE)
+                .data(UserMapper.INSTANCE.userDtoToUserUpdateOutput(userService.updateUserById(id, jwtToken, userUpdateInput)))
+                .status(HttpStatus.OK.toString())
+                .build();
 
         logger.info("[SUCCESS] Method updateCurrentUser in UserController completed successfully. Response: {}", response);
         return ResponseEntity.ok(response);
